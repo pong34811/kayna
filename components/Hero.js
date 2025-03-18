16d05210cd478eefa8a269234fd21510368432a2
watitnan.pc <pong34811@gmail.com>
2025-03-18 21:40:03 +0700

    refactor: clean up Hero component code and improve formatting

--- components/Hero.js
+++ components/Hero.js
@@ -1,52 +1,73 @@
-import React, { useState ,useEffect  } from 'react';
+import React, { useState, useEffect } from "react";
 import { FaYoutube, FaTiktok, FaDiscord, FaFacebook } from "react-icons/fa";
+import Image from "next/image";
 
 function Hero() {
-    const [channelData, setChannelData] = useState({
-      subscriberCount: 0,
-      videoCount: 0,
-      viewCount: 0,
-    });
-  
-    useEffect(() => {
-      fetch(
-        "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCC_P34t35REbiPzbHO_bifA&key=AIzaSyDcsfn7EkqklHYJwMS83-mfm7_O0uDHJAc"
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          const { subscriberCount, videoCount, viewCount } = data.items[0].statistics;
-          setChannelData({ subscriberCount, videoCount, viewCount });
-        })
-        .catch((error) => console.error("Error fetching data:", error));
-    }, []);
+  const [channelData, setChannelData] = useState({
+    subscriberCount: 0,
+    videoCount: 0,
+    viewCount: 0,
+  });
+
+  useEffect(() => {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCC_P34t35REbiPzbHO_bifA&key=AIzaSyDcsfn7EkqklHYJwMS83-mfm7_O0uDHJAc"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const { subscriberCount, videoCount, viewCount } =
+          data.items[0].statistics;
+        setChannelData({ subscriberCount, videoCount, viewCount });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-center">
       {/* Left Section: Image */}
-      <div className="w-full flex justify-center ">
-        <img
+      <div className=" flex justify-center ">
+        <Image
           src="/kayna.png"
           alt="Kayna Maid Hero"
-          className="object-cover "
+          className="object-cover"
+          width={2000}
+          height={2000}
         />
       </div>
 
       {/* Right Section: Text and Social Media */}
       <div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left space-y-6 mt-8 md:mt-0">
         {/* Text */}
-        <h1 className="text-4xl sm:text-5xl font-extrabold text-white drop-shadow-md">HI, I am Kayna</h1>
+        <h1 className="text-4xl sm:text-5xl font-extrabold text-white drop-shadow-md">
+          HI, I am Kayna
+        </h1>
+
         <p className="text-base sm:text-lg text-white leading-relaxed max-w-md">
-          I'm a VTuber.
-          <br />I'm a cute maid with a pretty face.
+          I&apos;m a VTuber.
+          <br />
+          I&apos;m a cute maid with a pretty face.
           <br />I love streaming games on YouTube!
         </p>
 
         {/* Social Media Logos */}
         <div className="flex space-x-3 sm:space-x-4 text-white text-3xl">
           {[
-            { href: "https://youtube.com", icon: <FaYoutube />, alt: "YouTube" },
+            {
+              href: "https://youtube.com",
+              icon: <FaYoutube />,
+              alt: "YouTube",
+            },
             { href: "https://tiktok.com", icon: <FaTiktok />, alt: "TikTok" },
-            { href: "https://discord.com", icon: <FaDiscord />, alt: "Discord" },
-            { href: "https://facebook.com", icon: <FaFacebook />, alt: "Facebook" },
+            {
+              href: "https://discord.com",
+              icon: <FaDiscord />,
+              alt: "Discord",
+            },
+            {
+              href: "https://facebook.com",
+              icon: <FaFacebook />,
+              alt: "Facebook",
+            },
           ].map(({ href, icon, alt }) => (
             <a
               key={alt}
